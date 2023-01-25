9dbf7a4b81f89a09b1ebe18b303cade950763652
JL <jay.lindsley@gmail.com>
2023-01-25 08:52:41 -0800

    docs: adding phpdoc blocks

--- src/BunnyPullZone.php
+++ src/BunnyPullZone.php
@@ -9,17 +9,39 @@ class BunnyPullZone
     protected string $pullzoneUrl;
     protected string $pullzoneSecurityToken;
 
+    /**
+     * Create a new BunnyPullZone, which represents a pull zone on BunnyCDN.
+     *
+     * @param string $pullzoneUrl The URL of the pull zone, including http:// or https://
+     * @param string $pullzoneSecurityToken If this pull zone is protected, then pass the security token here
+     */
     public function __construct(string $pullzoneUrl, string $pullzoneSecurityToken = '')
     {
         $this->pullzoneUrl = $pullzoneUrl;
         $this->pullzoneSecurityToken = $pullzoneSecurityToken;
     }
 
+    /**
+     * Returns the public URL for the target path behind the pull zone
+     *
+     * @param string $path
+     * @return string
+     */
     public function publicUrl(string $path): string
     {
         return rtrim($this->pullzoneUrl, '/').'/'.ltrim($path, '/');
     }
 
+    /**
+     * Returns a signed, temporary URL for an asset. Image optimization parameters are supported.
+     *
+     * @param string $path the path of the file to fetch
+     * @param DateTimeInterface $expiresAt expiration date of the signed url
+     * @param array $urlParameters optional parameters including: remote_ip, countries, & settings for bunny optimizer
+     * @param string $allowForPath pass an optional path to enable access to related objects that may be in the same folder. Unused, requires some fine tuning.
+     *
+     * @return string
+     */
     public function temporaryUrl(string $path, DateTimeInterface $expiresAt, array $urlParameters = [], string $allowForPath = ''): string
     {
         // bunny requires params in ascending order
